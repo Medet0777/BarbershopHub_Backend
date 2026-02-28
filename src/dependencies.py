0f4288163683260ff0956048cb16f4c84279672c
Medet0777 <muratbek_05@inbox.ru>
2026-03-01 02:17:42 +0500

    BBS-8:added sub dependency

--- src/dependencies.py
+++ src/dependencies.py
@@ -2,9 +2,19 @@ from fastapi import Query, Depends
 from typing import Annotated
 
 
+async def validate_limit(
+        limit: int = Query(100, ge=1, le=100, description="Max number of items to return")
+) -> int:
+    MAX_LIMIT = 100
+    return min(limit, MAX_LIMIT)
+
+
+LimitSubDependency = Annotated[int, Depends(validate_limit)]
+
+
 async def pagination(
         skip: int = Query(0, ge=0, description="Number of items to skip"),
-        limit: int = Query(100, ge=1, le=100, description="Max number of items to return")
+        limit: LimitSubDependency = 100
 ):
     return {"skip": skip, "limit": limit}
 
