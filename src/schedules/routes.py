0bc605a3c784b3adff70fbcb4def32450f2a5277
Medet0777 <muratbek_05@inbox.ru>
2026-03-01 03:34:29 +0500

    BBS-8:modified services

--- src/schedules/routes.py
+++ src/schedules/routes.py
@@ -1,30 +1,34 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from typing import List
 import uuid
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.dependencies import PaginationDependency, verify_admin_or_barbershop_staff_role
+from src.db.session import get_session
 from src.schedules import service
 from src.schedules.schemas import ScheduleOut, ScheduleCreate, ScheduleUpdate
 
 schedule_router = APIRouter()
 
 
-@schedule_router.get(
-    "/",
-    response_model=List[ScheduleOut]
-)
+@schedule_router.get("/", response_model=List[ScheduleOut])
 async def get_schedules(
-        pagination: PaginationDependency
+        pagination: PaginationDependency,
+        session: AsyncSession = Depends(get_session)
 ):
-    return service.get_all_schedules(skip=pagination["skip"], limit=pagination["limit"])
+    return await service.get_all_schedules(
+        skip=pagination["skip"],
+        limit=pagination["limit"],
+        session=session
+    )
 
 
-@schedule_router.get(
-    "/{schedule_id}",
-    response_model=ScheduleOut
-)
-async def get_schedule(schedule_id: uuid.UUID):
-    sched = service.get_schedule_by_id(schedule_id)
+@schedule_router.get("/{schedule_id}", response_model=ScheduleOut)
+async def get_schedule(
+        schedule_id: uuid.UUID,
+        session: AsyncSession = Depends(get_session)
+):
+    sched = await service.get_schedule_by_id(schedule_id, session)
     if not sched:
         raise HTTPException(status_code=404, detail="Schedule not found")
     return sched
@@ -36,8 +40,11 @@ async def get_schedule(schedule_id: uuid.UUID):
     status_code=status.HTTP_201_CREATED,
     dependencies=[Depends(verify_admin_or_barbershop_staff_role)]
 )
-async def create_schedule(sched_data: ScheduleCreate):
-    return service.create_schedule(sched_data)
+async def create_schedule(
+        sched_data: ScheduleCreate,
+        session: AsyncSession = Depends(get_session)
+):
+    return await service.create_schedule(sched_data, session)
 
 
 @schedule_router.patch(
@@ -45,8 +52,12 @@ async def create_schedule(sched_data: ScheduleCreate):
     response_model=ScheduleOut,
     dependencies=[Depends(verify_admin_or_barbershop_staff_role)]
 )
-async def update_schedule(schedule_id: uuid.UUID, update_data: ScheduleUpdate):
-    sched = service.update_schedule(schedule_id, update_data)
+async def update_schedule(
+        schedule_id: uuid.UUID,
+        update_data: ScheduleUpdate,
+        session: AsyncSession = Depends(get_session)
+):
+    sched = await service.update_schedule(schedule_id, update_data, session)
     if not sched:
         raise HTTPException(status_code=404, detail="Schedule not found")
     return sched
@@ -56,9 +67,12 @@ async def update_schedule(schedule_id: uuid.UUID, update_data: ScheduleUpdate):
     "/{schedule_id}",
     status_code=status.HTTP_204_NO_CONTENT,
     dependencies=[Depends(verify_admin_or_barbershop_staff_role)]
-
 )
-async def delete_schedule(schedule_id: uuid.UUID):
-    if not service.delete_schedule(schedule_id):
+async def delete_schedule(
+        schedule_id: uuid.UUID,
+        session: AsyncSession = Depends(get_session)
+):
+    deleted = await service.delete_schedule(schedule_id, session)
+    if not deleted:
         raise HTTPException(status_code=404, detail="Schedule not found")
     return {}
