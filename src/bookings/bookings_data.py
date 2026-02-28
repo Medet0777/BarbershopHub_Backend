fb267f1f8812896e2bc0769759f52ffcec0b2ef0
Medet0777 <muratbek_05@inbox.ru>
2026-03-01 00:00:14 +0500

    BBS-6:created needed files and data

---
+++ src/bookings/bookings_data.py
@@ -0,0 +1,27 @@
+# Фейковая база бронирований для теста
+import uuid
+from datetime import datetime, timezone
+from src.users.users_data import users
+from src.services.services_data import services
+from src.schedules.schedules_data import schedules
+
+bookings = [
+    {
+        "uid": uuid.uuid4(),
+        "user_id": users[0]["uid"] if users else uuid.uuid4(),
+        "service_id": services[0]["uid"] if services else uuid.uuid4(),
+        "schedule_id": schedules[0]["uid"] if schedules else uuid.uuid4(),
+        "status": "Pending",
+        "created_at": datetime.now(timezone.utc),
+        "updated_at": datetime.now(timezone.utc),
+    },
+    {
+        "uid": uuid.uuid4(),
+        "user_id": users[1]["uid"] if len(users) > 1 else uuid.uuid4(),
+        "service_id": services[1]["uid"] if len(services) > 1 else uuid.uuid4(),
+        "schedule_id": schedules[1]["uid"] if len(schedules) > 1 else uuid.uuid4(),
+        "status": "Confirmed",
+        "created_at": datetime.now(timezone.utc),
+        "updated_at": datetime.now(timezone.utc),
+    },
+]
