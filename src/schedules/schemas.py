ca673c0f72ccbed1982f98d19a9d245f1081b975
Medet0777 <muratbek_05@inbox.ru>
2026-02-28 23:49:35 +0500

    BBS-5:created pydantic for Schedule entity

---
+++ src/schedules/schemas.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from typing import Optional
+import uuid
+from datetime import datetime, time
+
+
+class ScheduleBase(BaseModel):
+    user_id: uuid.UUID
+    day_of_week: int  # 0 = Monday, 6 = Sunday
+    start_time: time
+    end_time: time
+
+
+class ScheduleCreate(ScheduleBase):
+    pass
+
+
+class ScheduleUpdate(BaseModel):
+    user_id: Optional[uuid.UUID]
+    day_of_week: Optional[int]
+    start_time: Optional[time]
+    end_time: Optional[time]
+
+
+class ScheduleOut(ScheduleBase):
+    uid: uuid.UUID
+    created_at: datetime
+    updated_at: datetime
+
+    class Config:
+        orm_mode = True
