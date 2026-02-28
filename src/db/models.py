bd153139a390920d621029060d97a51b4cee16e0
Medet0777 <muratbek_05@inbox.ru>
2026-02-28 21:59:42 +0500

    BBS-3:added barbershop and service models

--- src/db/models.py
+++ src/db/models.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime, timezone
+from typing import Optional
 
 import sqlalchemy.dialects.postgresql as pg
 from sqlalchemy import ForeignKey, Integer, Time
@@ -109,6 +110,62 @@ class Schedule(SQLModel, table=True):
     day_of_week: int = Field(sa_column=Column(Integer, nullable=False))  # 0=Monday, 6=Sunday
     start_time: str = Field(sa_column=Column(Time, nullable=False))
     end_time: str = Field(sa_column=Column(Time, nullable=False))
+
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP(timezone=True), default=lambda: datetime.now(timezone.utc))
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP(timezone=True), default=lambda: datetime.now(timezone.utc),
+                         onupdate=lambda: datetime.now(timezone.utc))
+    )
+
+
+# Service model part
+class Service(SQLModel, table=True):
+    __tablename__ = "services"
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            default=uuid.uuid4
+        )
+    )
+
+    name: str
+    description: Optional[str] = None
+    category: str
+    duration_minutes: int = Field(default=30)
+    price: float = Field(default=0)
+
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP(timezone=True), default=lambda: datetime.now(timezone.utc))
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP(timezone=True), default=lambda: datetime.now(timezone.utc),
+                         onupdate=lambda: datetime.now(timezone.utc))
+    )
+
+
+class Barbershop(SQLModel, table=True):
+    __tablename__ = "barbershops"
+
+    uid: uuid.UUID = Field(
+        sa_column=Column(
+            pg.UUID,
+            primary_key=True,
+            unique=True,
+            nullable=False,
+            default=uuid.uuid4
+        )
+    )
+
+    name: str
+    address: str
+    phone: str
+    email: str
 
     created_at: datetime = Field(
         sa_column=Column(pg.TIMESTAMP(timezone=True), default=lambda: datetime.now(timezone.utc))
